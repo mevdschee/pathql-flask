9396746a184c6ebb9818d0ef6d1d798a2aa5d569
Maurits van der Schee <maurits@vdschee.nl>
2019-11-03 19:47:42 +0100

    add auth

--- pathql.py
+++ pathql.py
@@ -14,19 +14,38 @@ Swagger(app, template={
     "title": "PathQL API",
     "description": "PathQL API for my data",
     "version": "0.9.1"
-  }
+  },
+  "securityDefinitions": {
+    "basicAuth": {
+      "type": "basic"
+    }
+  },
+  "security": [{      
+      "basicAuth": []
+    }
+  ]
 })
 
 app.config['JSON_ADD_STATUS'] = False
-db = None
+config = None
 cwd = getcwd()
 
 @app.route('/pathql', methods=['POST'])
 @swag_from(cwd+'/pathql.yml')
 @as_json
 def pathql():
-    data = request.get_json(force=True)
     try:    
+        username = config.get("flask", "username")
+        password = config.get("flask", "password")
+        if request.authorization:
+            username = request.authorization.username
+            password = request.authorization.password
+        database = config.get("flask", "database")
+        driver = config.get("flask", "driver")
+        address = config.get("flask", "address")
+        port = config.get("flask", "port")
+        db = PathAlchemy.create(username,password,database,driver,address,port)
+        data = request.get_json(force=True)
         result = db.path_query(data['query'],data['params'])
     except Exception as e:
         result = json_response(500, type=type(e).__name__, message=str(e))
@@ -35,11 +54,4 @@ def pathql():
 if __name__ == '__main__':
     config = ConfigParser()
     config.read("config.ini")
-    username = config.get("flask", "username")
-    password = config.get("flask", "password")
-    database = config.get("flask", "database")
-    driver = config.get("flask", "driver")
-    address = config.get("flask", "address")
-    port = config.get("flask", "port")
-    db = PathAlchemy.create(username,password,database,driver,address,port)
     app.run()
